1981a1b6111628db0237c07536b698c887e5b9a5
Sebastian Helzle <sebastian@helzle.it>
2025-12-17 16:34:26 +0100

    TASK: Solve linting issue

--- ui-plugin/src/helpers/hooks.ts
+++ ui-plugin/src/helpers/hooks.ts
@@ -4,7 +4,7 @@ import { useRef, useEffect, useCallback } from 'react';
  * A hook that returns a memoized callback which always has the latest version of the handler.
  * This prevents triggering effects or re-renders when the handler changes.
  */
-export function useEventCallback<T, K>(handler?: (value: T, event: K) => void): (value: T, event: K) => void {
+export function useEventCallback<T, K>(handler?: (value: T, event?: K) => void): (value: T, event?: K) => void {
     const callbackRef = useRef(handler);
 
     useEffect(() => {
